857cf607d02c6c746b3abe1d3b2765b22c688889
learntodroid <56079350+learntodroid@users.noreply.github.com>
2020-03-04 19:47:29 +1100

    Update CommentsService.java

--- app/src/main/java/com/learntodroid/postrequestwithjson/CommentsService.java
+++ app/src/main/java/com/learntodroid/postrequestwithjson/CommentsService.java
@@ -2,7 +2,6 @@ package com.learntodroid.postrequestwithjson;
 
 import java.util.Map;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
